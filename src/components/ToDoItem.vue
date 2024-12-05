bc7363e756f216edbce0d7c86949be323cd5b1e3
zyb1412 <1412850451@qq.com>
2024-12-05 10:58:10 +0800

    '提交初始代码'

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="stack-small" v-if="!isEditing">
+    <div class="custom-checkbox">
+      <input
+        type="checkbox"
+        class="checkbox"
+        :id="id"
+        :checked="isDone"
+        @change="$emit('checkbox-changed')" />
+      <label :for="id" class="checkbox-label">{{label}}</label>
+    </div>
+    <div class="btn-group">
+<button
+  type="button"
+  class="btn"
+  ref="editButton"
+  @click="toggleToItemEditForm">
+  Edit
+  <span class="visually-hidden">{{label}}</span>
+</button>
+
+      <button type="button" class="btn btn__danger" @click="deleteToDo">
+        Delete <span class="visually-hidden">{{label}}</span>
+      </button>
+    </div>
+  </div>
+  <to-do-item-edit-form
+  v-else
+  :id="id"
+  :label="label"
+  @item-edited="itemEdited"
+  @edit-cancelled="editCancelled">
+</to-do-item-edit-form>
+
+
+</template>
+
+
+<script>
+import ToDoItemEditForm from "./ToDoItemEditForm";
+
+
+export default {
+    components: {
+  ToDoItemEditForm
+},
+
+  props: {
+    label: { required: true, type: String },
+    done: { default: false, type: Boolean },
+    id: { required: true, type: String },
+  },
+  data() {
+    return {
+      
+      isEditing: false
+    };
+  },
+  computed: {
+  isDone() {
+    return this.done;
+  }
+},
+
+  methods: {
+    deleteToDo() {
+      this.$emit('item-deleted');
+    },
+    itemEdited(newLabel) {
+  this.$emit('item-edited', newLabel);
+  this.isEditing = false;
+  this.focusOnEditButton();
+},
+editCancelled() {
+  this.isEditing = false;
+  this.focusOnEditButton();
+},
+
+toggleToItemEditForm() {
+  console.log(this.$refs.editButton);
+  this.isEditing = true;
+},
+focusOnEditButton() {
+  this.$nextTick(() => {
+    const editButtonRef = this.$refs.editButton;
+    editButtonRef.focus();
+  });
+}
+
+
+
+  }
+
+};
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production' 
+    ? '/zybvue/'  // GitHub Pages 上的路径
+    : '/'  // 本地开发时的路径
+});
