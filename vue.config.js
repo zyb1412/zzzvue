9388224e266be54ab13fd8852fea12ed162051c5
zyb1412 <1412850451@qq.com>
2024-12-05 15:26:31 +0800

    '码'

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production' 
-    ? '/zybvue/'  // GitHub Pages 上的路径
+    ? '/zzzvue/'  // GitHub Pages 上的路径
     : '/'  // 本地开发时的路径
 });
